f238eabef1f2a5b6d8cf4c232fbd41d07681bdd0
nimanthaprasad <103378190+nimanthaprasad@users.noreply.github.com>
2023-03-08 20:41:22 +0530

    first commit

---
+++ home.php
@@ -0,0 +1,116 @@
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="home.css">
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="nav">
+    
+        <label class="logo"> W-school </label>
+            <ul>
+                <li> <a href="">Home</a> </li>
+                <li> <a href="">Contact</a> </li>
+                <li> <a href="">Admission</a> </li>
+                <li> <a href="" class="btn btn-success">Login </a> </li>
+            </ul>
+    
+</div>
+
+  <div class="section1"></div>
+  <label class="image_test">We Teach Students With Care</label>
+        <img class="main_img" src="school_management.jpg">
+  </div>
+  <div class="container">
+    <div class="row">
+        <div class="col-md-4">
+             <img class="welcome_img" src="school2.jpg" alt="">
+        </div>
+        <div class="col-md-8">
+             <h1>Welcome To W-School</h1>
+             <p>Bootstrap’s grid includes five tiers of predefined classes for building complex responsive layouts. Customize the size of your columns on extra small, small, medium, large, or extra large devices however you see fit.</p>
+
+        </div>
+        
+    </div>
+    
+
+  </div>
+  <center>
+
+    <h1 class="Teachers">Our Teachers</h1>
+            
+    </center>
+    <div class="container">
+
+        <div class"row">
+
+            <div  class="col-md-4">
+                  <img class="teacher" src="teacher1.jpg" alt="">
+                  <p>For example, here are two grid layouts that apply to every device and viewport, from xs to xl. Add any number of unit-less classes for each breakpoint you need and every column will be the same width.</p>
+            </div>
+            <div class="col-md-4">
+                <img class="teacher" src="teacher2.jpg" alt="">
+                <p>For example, here are two grid layouts that apply to every device and viewport, from xs to xl. Add any number of unit-less classes for each breakpoint you need and every column will be the same width.</p>
+            </div>
+            <div class="col-md-4">
+                <img class="teacher" src="teacher3.jpg" alt="">
+                <p>For example, here are two grid layouts that apply to every device and viewport, from xs to xl. Add any number of unit-less classes for each breakpoint you need and every column will be the same width.</p>
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="container">
+
+        <div class"row">
+
+            <div  class="col-md-4">
+                  <img class="teacher" src="web.jpg" alt="">
+                  <h6>Wen Developer</h6>
+            </div>
+            <div class="col-md-4">
+                <img class="teacher" src="marketing.png" alt="">
+                <h6>Marketing</h6>
+            </div>
+            <div class="col-md-4">
+                <img class="teacher" src="graphic.jpg" alt="">
+                <h6>Graphic Design</h6>
+            </div>
+
+        </div>
+
+    </div>
+    <center>
+        <h1>Addmition Form</h1>
+    </center>
+    <div align="center" class="Admition_form">
+        <form>
+            <div>
+                <label class="label_text">Name</label>
+                <input class="input_deg" type="text" name="">
+            </div>
+            <div>
+                <label class="label_text"> Email</label>
+                <input type="text" name="">
+            </div>
+            <div>
+                <label class="label_text"> phone</label>
+                <input type="text" name="" id="">
+            </div>
+            <div>
+                <label class="label_text">Message</label>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+            </div>
+        </form>
+    </div>
+    
+</body>
+</html>
